07d06f651978b7333040298c5198def779583dc2
Dmytro Ovcharenko <ovch.dmytro.de@gmail.com>
2025-07-14 13:51:34 +0200

    Zwei felde

--- navbar.js
+++ navbar.js
@@ -7,9 +7,21 @@ export function renderNavbar() {
   const container = document.createElement("div");
   container.className = "flex justify-between items-center max-w-5xl mx-auto";
 
-  const logo = document.createElement("span");
-  logo.textContent = "Movie Diary";
-  logo.className = "text-2xl font-bold tracking-wide";
+
+  const logo = document.createElement("div");
+  logo.className = "flex items-center gap-2";
+
+  const logoImg = document.createElement("img");
+  logoImg.src = "logo.png"; 
+  logoImg.alt = "Movie Diary Logo";
+  logoImg.className = "h-14 w-14"; 
+
+  const logoText = document.createElement("span");
+  logoText.textContent = "Movie Diary";
+  logoText.className = "text-2xl font-bold tracking-wide";
+
+  logo.appendChild(logoImg);
+  logo.appendChild(logoText);
 
   const button = document.createElement("button");
   button.className =
